d4774506da774464ce1b4213fc1ea51ef13524b4
LoïcG <gerbaudloic@gmail.com>
2012-03-03 12:24:43 +0100

    Added constants : LANG and LANG_DIR

--- wp_multisite_cron.php
+++ wp_multisite_cron.php
@@ -7,19 +7,32 @@ Author: LoicG
 Author URI: http://blog.loicg.net/
  */
 
+register_activation_hook(__FILE__, array('wp_multisite_cron','plugin_activation'));
 add_action('init', array('wp_multisite_cron', 'init'));
 
 class wp_multisite_cron{
 	
+	const LANG = 'wp_multisite_cron';
+	const LANG_DIR = '/lang/';
+	
+	/**
+	 * 
+	 * Plugin activation (sets default parameters)
+	 */
+	public static function plugin_activation(){
+    	if(!self::get_option('concurrent_crons'))
+    		self::update_option('concurrent_crons', 100);
+    }
+    
 	/**
 	 * 
 	 * The main 'loader'
 	 */
-	function init() {
+	public static function init() {
 
 		//Setup the translation
-		load_plugin_textdomain('wp_multisite_cron',false, dirname(plugin_basename( __FILE__ ) ) . '/lang/');
-		
+		load_plugin_textdomain(self::LANG, false, dirname(plugin_basename( __FILE__ ) ) . self::LANG_DIR);
+
 		//The multisite cron action		
 		add_action('wp_ajax_wp_multisite_cron_call', array('wp_multisite_cron','cron_call'));
 		add_action('wp_ajax_nopriv_wp_multisite_cron_call', array('wp_multisite_cron','cron_call'));
@@ -74,45 +87,74 @@ class wp_multisite_cron{
 			</div>
     		<h2>WordPress multisite cron</h2>
     		
-    		<?php if (isset($_POST) && !empty($_POST)) :
-
-	        	//Let's save some actions ...
-	        
-	            ?>
-	            <div id="setting-error-settings_updated" class="updated settings-error">
-					<p>
-						<strong><?php _e('Settings saved.')?></strong>
-					</p>
-				</div>
-	            
+    		<?php if (isset($_POST['wp_multisite_cron']) && !empty($_POST['wp_multisite_cron'])) :
+	
+				if (isset($_POST['_wpnonce']) && wp_verify_nonce( $_POST['_wpnonce'], plugin_basename( __FILE__ ) )) :
+		        	self::delete_option('concurrent_crons');
+		        	
+		        	foreach($_POST['wp_multisite_cron'] as $option_name=>$option_value){
+		        		self::update_option($option_name, $option_value);
+		        	}
+	        	?>
+		            <div id="setting-error-settings_updated" class="updated settings-error">
+						<p>
+							<strong><?php _e('Settings saved.')?></strong>
+						</p>
+					</div>
+				<?php else: ?>
+					<div id="message" class="error fade">
+						<p><?php _e('Unable to update settings.',self::LANG)?></p>
+					</div>
+				<?php endif;?>
 			<?php endif;?>
     		
     		
     		
 			<?php if(!is_multisite()):?>
 				<div class="tool-box error">
-					<h3><?php _e('Multisite','wp_multisite_cron') ?></h3>
-					<p><?php _e('Your WordPress installation is not configured as multisite','wp_multisite_cron') ?>.</p>
-					<p><?php _e("Read the WordPress' documentation about multisite to learn how to set up multisite",'wp_multisite_cron')?> : <a href="http://codex.wordpress.org/Create_A_Network" target="_blank"><?php _e('here','wp_multisite_cron')?></a></p>
+					<h3><?php _e('Multisite',self::LANG) ?></h3>
+					<p><?php _e('Your WordPress installation is not configured as multisite',self::LANG) ?>.</p>
+					<p><?php _e("Read the WordPress' documentation about multisite to learn how to set up multisite",self::LANG)?> : <a href="http://codex.wordpress.org/Create_A_Network" target="_blank"><?php _e('here',self::LANG)?></a></p>
 				</div>
 			<?php endif;?>
 									
 						
 			<?php if(!defined('DISABLE_WP_CRON') || !DISABLE_WP_CRON ):?>
 				<div class="tool-box error">
-					<h3><?php _e('WordPress default cron system','wp_multisite_cron') ?></h3>
-					<p><?php _e('You must disable the WordPress cron system, to use this extension','wp_multisite_cron') ?>.</p>
-					<p><?php _e('Add the following code in your wp-config.php','wp_multisite_cron') ?> :</p>
+					<h3><?php _e('WordPress default cron system',self::LANG) ?></h3>
+					<p><?php _e('You must disable the WordPress cron system, to use this extension',self::LANG) ?>.</p>
+					<p><?php _e('Add the following code in your wp-config.php',self::LANG) ?> :</p>
 					<p>
 						<code>define('DISABLE_WP_CRON', true);</code>
 					</p>
 				</div>
 			<?php endif;?>
 			
+			<form action="" method="post">
+	    		<table class="form-table">
+					<tbody>
+						<tr valign="top">
+							<th scope="row">
+								<label for="concurrent_crons"><?php _e('Concurrent crons',self::LANG) ?></label><br />
+								<em><?php _e("Lower it if some of your sites' cron don't run",self::LANG)?></em>
+							</th>
+							<td>
+								<input type="text" name="wp_multisite_cron[concurrent_crons]" id="concurrent_crons" value="<?php echo self::get_option('concurrent_crons') ?>" />
+							</td>
+						</tr>
+					</tbody>
+				</table>
+				<p class="submit">
+	            	<input class="button-primary" name="plugin_ok" value="<?php _e('Save settings',self::LANG) ?>" type="submit" />
+	            </p>
+	            <?php wp_nonce_field( plugin_basename( __FILE__ ), '_wpnonce' );?>
+			</form>
+			
+			
 			<?php if(self::is_ready()): ?>
 				<div class="tool-box">
-					<h3><?php _e('Set-up WordPress multisite cron','wp_multisite_cron') ?></h3>
-					<p><?php _e('Add the following line in your crontab','wp_multisite_cron')?> :</p>
+					<h3><?php _e('Set-up WordPress multisite cron',self::LANG) ?></h3>
+					<p><?php _e('Add the following line in your crontab',self::LANG)?> :</p>
 					<p>
 						<code>*/15 *<?php echo "\t" ?>* * * www-data /usr/bin/wget -qO- <?php echo admin_url('admin-ajax.php','http').'?action=wp_multisite_cron_call' ?></code>
 					</p>
@@ -142,24 +184,33 @@ class wp_multisite_cron{
     	if(count($blogs)){
     		$mh = curl_multi_init();
 	    	
-	    	foreach($blogs as &$blog_id){
-	    		switch_to_blog($blog_id);
-	    		$cron_url = site_url().'/wp-cron.php?doing_wp_cron';
-
-	    		//Add the url to the stack
-	    		$ch=curl_init();
-	    		curl_setopt($ch, CURLOPT_URL, $cron_url);
-	    		curl_setopt($ch, CURLOPT_HEADER, 0);
-	    		curl_multi_add_handle($mh, $ch);
-	    	}
-    	
-	    	//Launch :)
-	    	$running = null;
-	    	do{
-	    		curl_multi_exec($mh, $still_running);
-	    	}while($still_running > 0);
+    		$length = self::get_option('concurrent_crons');
+	    	$offset = 0;
 	    	
-	    	curl_multi_close($mh);
+	    	//Cut the 
+	    	while($blogs_slice = arrray_slice($blogs,$offset,$length)){
+    		
+	    		$offset += $length;
+	    		
+		    	foreach($blogs_slice as &$blog_id){
+		    		switch_to_blog($blog_id);
+		    		$cron_url = site_url().'/wp-cron.php?doing_wp_cron';
+	
+		    		//Add the url to the stack
+		    		$chs[$blog_id]=curl_init();
+		    		curl_setopt($chs[$blog_id], CURLOPT_URL, $cron_url);
+		    		curl_setopt($chs[$blog_id], CURLOPT_HEADER, 0);
+		    		curl_multi_add_handle($mh, $ch);
+		    	}
+	    	
+		    	//Launch :)
+		    	$running = null;
+		    	do{
+		    		curl_multi_exec($mh, $still_running);
+		    	}while($still_running > 0);
+		    	
+		    	curl_multi_close($mh);
+		    }
     	}
     	die();
     }
